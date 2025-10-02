5397a4a70311f85796489b48b0041eb0da4a56ff
mirko-geno <mirkogenovesi24@gmail.com>
2025-10-02 16:55:46 -0300

    Changed from dmp Quaternion to Raw data

--- firmware/src/mpu.rs
+++ firmware/src/mpu.rs
@@ -11,8 +11,6 @@ use embassy_sync::{
 use mpu6050_dmp::{
     sensor_async::Mpu6050,
     calibration::CalibrationParameters,
-    quaternion::Quaternion,
-    yaw_pitch_roll::YawPitchRoll,
 };
 use heapless::String;
 use core::fmt::Write;
@@ -21,9 +19,9 @@ use crate::{MESSAGE_LENGTH, CHANNEL_SIZE};
 
 async fn calibrate_sensor(mpu: &mut Mpu6050<I2c<'static, I2C0, i2c::Async>>) {
     let calibration_params = CalibrationParameters::new(
-        mpu6050_dmp::accel::AccelFullScale::G4,
+        mpu6050_dmp::accel::AccelFullScale::G2,
         mpu6050_dmp::gyro::GyroFullScale::Deg2000,
-        mpu6050_dmp::calibration::ReferenceGravity::ZP,
+        mpu6050_dmp::calibration::ReferenceGravity::Zero,
     );
 
     log::info!("Calibrating Sensor");
@@ -52,53 +50,30 @@ pub async fn read_mpu(mut mpu: Mpu6050<I2c<'static, I2C0, i2c::Async>>, tx_ch: S
     // Calibrate sensor
     calibrate_sensor(&mut mpu).await;
 
-    // Configure DMP update rate
-    mpu.set_sample_rate_divider(2).await.unwrap(); // 2 for good motion tracking
-    log::info!("Sample rate configured");
-
-
-    // Enable FIFO for quaternion data
-    mpu.enable_fifo().await.unwrap();
-    // Buffer for FIFO data (DMP packets are 28 bytes)
-    let mut buffer = [0u8; 28];
-
     // Main loop reading quaternion data
     loop {
-        let fifo_count = mpu.get_fifo_count().await.unwrap();
-
-        if fifo_count >= 28 {
-            // Read a complete DMP packet
-            let data = mpu.read_fifo(&mut buffer).await.unwrap();
-
-            // First 16 bytes contain quaternion data
-            // The quaternion represents the sensor's orientation in 3D space:
-            // - w: cos(angle/2) - indicates amount of rotation
-            // - i,j,k: axis * sin(angle/2) - indicates rotation axis
-            let quat = Quaternion::from_bytes(&data[..16]).unwrap().normalize();
-
-            // Convert quaternion to more intuitive Yaw, Pitch, Roll angles
-            // Note: angles are in radians (-π to π)
-            let ypr = YawPitchRoll::from(quat);
-
-            // Convert radians to degrees for more intuitive reading
-            let yaw_deg = ypr.yaw * 180.0 / core::f32::consts::PI;
-            let pitch_deg = ypr.pitch * 180.0 / core::f32::consts::PI;
-            let roll_deg = ypr.roll * 180.0 / core::f32::consts::PI;
-
-            /*
-            In this part of the code the magnetometer data
-            should be added to z component (k) of the quaternion
-            */           
-
-            // Display quaternion components
-            // Values are normalized (sum of squares = 1)
-            let mut message: String<MESSAGE_LENGTH> = String::new();
-            write!(&mut message, "\nAngles: y={}, p={:.3}, r={:.3}", yaw_deg, pitch_deg, roll_deg).unwrap();
-            tx_ch.send(message).await;
-            log::info!("\nAngles: y={}, p={:.3}, r={:.3}", yaw_deg, pitch_deg, roll_deg);
-
-
-        }
+        let (accel, gyro) = (
+            mpu.accel().await.unwrap(),
+            mpu.gyro().await.unwrap()
+        );
+        log::info!("Sensor Readings:");
+        log::info!(
+            "  Accelerometer [mg]: x={}, y={}, z={}",
+            accel.x(),
+            accel.y(),
+            accel.z()
+        );
+        log::info!(
+            "  Gyroscope [deg/s]: x={}, y={}, z={}",
+            gyro.x(),
+            gyro.y(),
+            gyro.z()
+        );
+        let mut message: String<MESSAGE_LENGTH> = String::new();
+        write!(&mut message,
+            "\nAcc: x={}, y={}, z={}", accel.x(), accel.y(), accel.z()
+        ).unwrap();
+        tx_ch.send(message).await;
         Timer::after_millis(100).await;
     }
 }
