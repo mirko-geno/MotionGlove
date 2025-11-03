bb76121fa6478d5d5d7715aca5ff30fcf7d53272
mirko-geno <mirkogenovesi24@gmail.com>
2025-11-03 10:52:04 -0300

    Propietary router connection

--- shared/src/definitions.rs
+++ shared/src/definitions.rs
@@ -1,12 +1,12 @@
 use embassy_time::Duration;
 
-pub const WIFI_NETWORK: &str = "MirkoWifi";
-pub const WIFI_PASSWORD: &str = "password123";
+pub const WIFI_NETWORK: &str = "MotionGlove-Network";
+pub const WIFI_PASSWORD: &str = "Password123";
 pub const TCP_CHANNEL: u8 = 5;
 pub const TCP_ENDPOINT: u16 = 50124;
 pub const SOCKET_TIMEOUT: Duration = Duration::from_secs(15);
-pub const DONGLE_IP: &str = "169.254.1.1";
-pub const GLOVE_IP: &str = "169.254.1.2";
+pub const DONGLE_IP: &str = "192.168.0.10";
+pub const GLOVE_IP: &str = "192.168.0.12";
 pub const CHANNEL_SIZE: usize = 1;
 pub const READ_FREQ: u64 = 1000;
 pub const DELTA_TIME: f32 = 1.0 / READ_FREQ as f32;
